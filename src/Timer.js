4e0b7e789adc22bad6f69c5f594fb2d899675f49
Holger Mueller <9jholger@gmail.com>
2020-06-19 13:29:30 -0500

    worked with moment

--- src/Timer.js
+++ src/Timer.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Moment from "react-moment";
+import moment from "moment";
 
 export default class TimerDisplay extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      todaysDate: moment().format("MMMM DD, YYYY"),
+      ago: moment().subtract(136, "years"),
       time: "",
     };
   }
@@ -16,8 +19,15 @@ export default class TimerDisplay extends Component {
         className="timer"
         style={{ width: "55%", margin: "2% auto", textAlign: "center" }}
       >
+        <h1>Since April 9, 1865</h1>
+        <h1>Since June 19, 1865</h1>
+
+        <h1>Since December 6, 1865</h1>
         <h1>00:00:00</h1>
-        <Moment format="MMMM Do, YYYY" />
+        <Moment fromNow ago>
+          {this.state.ago}
+        </Moment>
+        <div>{this.state.todaysDate.toLocaleString()}</div>
         <h2>Have passed.</h2>
       </div>
     );
