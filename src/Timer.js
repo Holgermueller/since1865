45fe191e85c601c9091d017fc99d6ca319ca398d
Holger Mueller <9jholger@gmail.com>
2020-06-20 10:45:19 -0500

    worked on getting difference in dates

--- src/Timer.js
+++ src/Timer.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Moment from "react-moment";
+//import Moment from "react-moment";
 import moment from "moment";
 
 export default class TimerDisplay extends Component {
@@ -7,11 +7,20 @@ export default class TimerDisplay extends Component {
     super(props);
 
     this.state = {
+      then: "April 09, 1865",
       todaysDate: moment().format("MMMM DD, YYYY"),
-      ago: moment().subtract(136, "years"),
-      time: "",
     };
   }
+
+  componentDidMount = () => {
+    return this.getDurationInYears();
+  };
+
+  getDurationInYears = () => {
+    moment(this.state.todaysDate, "MMMM DD, YYYY").diff(
+      moment(this.state.then, "MMMM DD, YYYYH")
+    );
+  };
 
   render() {
     return (
@@ -24,11 +33,9 @@ export default class TimerDisplay extends Component {
 
         <h1>Since December 6, 1865</h1>
         <h1>00:00:00</h1>
-        <Moment fromNow ago>
-          {this.state.ago}
-        </Moment>
+        <h1>{this.getDurationInYears()} have passed</h1>
         <div>{this.state.todaysDate.toLocaleString()}</div>
-        <h2>Have passed.</h2>
+        {/* <h2>Have passed.</h2> */}
       </div>
     );
   }
